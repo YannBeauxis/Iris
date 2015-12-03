a481fda60d8f2c21a3cefc55372057a97d2dbb5e
Yann Beauxis <yann.beauxis@gmail.com>
2015-12-03 23:42:30 +0100

    New design with BootStrap !!

--- Gemfile
+++ Gemfile
@@ -11,6 +11,9 @@ gem 'sass-rails', '~> 5.0'
 #gem 'compass-rails'
 gem "compass-rails", github: "Compass/compass-rails", branch: "master"
 gem 'compass-normalize', '~> 1.5'
+#Bootstrap
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- app/helpers/menus_helper.rb
+++ app/helpers/menus_helper.rb
@@ -17,7 +17,21 @@ module MenusHelper
     #end
     if show then
       content_for options[:menu] do
-        content_tag(:span, link_to(options[:label], path, options[:link_param]), class: "menu-link")
+        if options[:menu] == :nav_resume
+          if options[:active]
+            content_tag(:li, options[:label], class: "active")    
+          else
+            content_tag(:li, link_to(options[:label], path, options[:link_param]))    
+          end
+        elsif options[:menu] == :content_menu
+          opt = options[:link_param] ||= {}
+          opt[:class] = "btn btn-default"
+          opt[:role] = :button        
+          link_to(options[:label], path, opt)    
+          #content_tag(:button, options[:label], type: "button", class: "list-group-item")    
+        else
+          content_tag(:span, link_to(options[:label], path, options[:link_param]), class: "menu-link")
+        end
       end
     end
   end
