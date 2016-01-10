90a8ffc190bf6d8dbe1ab6abc767bb0f53cee298
Yann Beauxis <yann.beauxis@gmail.com>
2016-01-10 23:40:40 +0100

    debug production date display

--- app/assets/javascripts/models/Product.js
+++ app/assets/javascripts/models/Product.js
@@ -1,5 +1,10 @@
 App.Models.Product = Backbone.Model.extend({
   
-  initialize: function() {this.url = this.collection.url;}
+  initialize: function() {this.url = this.collection.url;},
+
+  productionDateDisplay: function() {
+    var date = this.get('production_date');
+    return (date == null) ? date : date.slice(0,10);
+  }
 
 });
