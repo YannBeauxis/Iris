d7129e7f29d6b8bcaabdcacb89d1f89217a35e73
Yann Beauxis <yann.beauxis@gmail.com>
2016-05-02 20:42:11 +0200

    new user with limited roles options. devise view design

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def new
+    @user = User.new
+    @roles = Role.where("rank >= 3")
+    sub_text = {3 => 'création de recettes', 4 => 'consultation uniquement'}
+    @roles.each { |r| r.name += ' (' + sub_text[r.rank] + ')' }
+    super
+  end
+
   private
 
   def sign_up_params
@@ -9,4 +17,5 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:name,:role_id, :email, :password, :password_confirmation, :current_password)
   end
+  
 end
