57f4747c3a09ca3e6213815c9f4806f02e7eebf9
Yann Beauxis <yann.beauxis@gmail.com>
2016-06-06 20:53:09 +0200

    debug roles select on redirecte after create error

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def new
-    @user = User.new
-    @roles = Role.where("rank >= 3")
-    sub_text = {3 => 'création de recettes', 4 => 'consultation uniquement'}
-    @roles.each { |r| r.name += ' (' + sub_text[r.rank] + ')' }
+    init_roles
+    super
+  end
+  
+  def create
+    init_roles
     super
   end
 
@@ -16,6 +18,12 @@ class RegistrationsController < Devise::RegistrationsController
 
   def account_update_params
     params.require(:user).permit(:name,:role_id, :email, :password, :password_confirmation, :current_password)
+  end
+  
+  def init_roles
+    @roles = Role.where("rank >= 3")
+    sub_text = {3 => 'création de recettes', 4 => 'consultation uniquement'}
+    @roles.each { |r| r.name += ' (' + sub_text[r.rank] + ')' }
   end
   
 end
