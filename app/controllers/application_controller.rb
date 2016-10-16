fa05758bc702d24aa9c621a493d6035ffcebb2cc
Yann Beauxis <mail@yannbeauxis.net>
2016-10-16 13:29:37 +0200

    force_ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
   
+  force_ssl if: :ssl_configured?
+
+  def ssl_configured?
+    !Rails.env.development?
+  end
+
   before_filter :authenticate_user!
 
   check_authorization :unless => :devise_controller?
