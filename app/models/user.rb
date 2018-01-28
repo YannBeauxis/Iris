639e76a04e5fe2b9b6eeb2183d500146372f6088
Yann Beauxis <mail@yannbeauxis.net>
2018-01-28 16:11:58 +0100

    disable user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
-          :registerable,
+          #:registerable,
           :recoverable, 
          :rememberable, :trackable, :validatable
   before_create :set_default_info
