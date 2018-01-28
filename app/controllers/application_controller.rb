64108d7e8f7b5290f6fc857d362f222b40cfeb7a
Yann Beauxis <mail@yannbeauxis.net>
2018-01-28 16:21:48 +0100

    disable force ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
   
-  force_ssl if: :ssl_configured?
+  #force_ssl if: :ssl_configured?
 
   def ssl_configured?
     !Rails.env.development? && !Rails.env.test?
